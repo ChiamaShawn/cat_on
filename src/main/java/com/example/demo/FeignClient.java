f080f32201d694004aaa747c2956a50413cc455f
System Administrator <root@SageArts-MacBook-Air.local>
2019-09-25 17:15:03 +0300

    Initial commit

--- src/main/java/com/example/demo/FeignClient.java
+++ src/main/java/com/example/demo/FeignClient.java
@@ -1,34 +1,22 @@
 package com.example.demo;
 
-import com.example.demo.models.Appointment;
-import com.example.demo.models.Lecturer;
+import com.example.demo.models.Attachment;
 import com.example.demo.models.Student;
-import feign.Body;
-import org.hibernate.annotations.common.reflection.XMethod;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
-@org.springframework.cloud.openfeign.FeignClient(name = "client", url = "http://127.0.0.1:1000")
+@org.springframework.cloud.openfeign.FeignClient(name = "client", url = "http://10.51.10.111:2000",configuration = FeignConfig.class)
 public interface FeignClient {
 
     //Register Student
     @RequestMapping(method = RequestMethod.POST, value = "students")
     Student registerStudent(@RequestBody Student student);
 
-    //Get registered student
-    @RequestMapping(method = RequestMethod.GET, value = "students")
-    Student findByName(@RequestParam(name = "studentNumber") String studentNumber);
+    @RequestMapping(method = RequestMethod.POST, value= "companies/7/attachments")
+    Attachment createAttachment(@RequestParam(name = "studentId") Long studentId);
 
-    //Get lecturers
-    @RequestMapping(method = RequestMethod.GET, value = "lecturers")
-    List<Lecturer> getAll();
+    @RequestMapping(method = RequestMethod.PATCH, value= "companies/7/attachments")
+    Attachment deleteAttachment(@RequestParam(name = "studentId") Long studentId, @RequestParam(name = "departmentId") Long departmentId , @RequestParam(name = "companyId") Long companyId  );
 
-    //Create Appointment
-    @RequestMapping(method = RequestMethod.POST, value = "appointments")
-    Appointment book(@RequestBody Appointment appointment);
-
-    //Confirm Booking
-    @RequestMapping(method = RequestMethod.PATCH, value = "appointments/{appointmentId}")
-    Appointment confirm(@RequestParam(name = "studentId") Long studentId, @PathVariable(name = "appointmentId") Long appointmentId);
+    @RequestMapping(method = RequestMethod.DELETE, value= "departments")
+    Attachment patchAttachment(@RequestParam(name = "studentId") Long studentId, @RequestParam(name = "departmentId") Long departmentId );
 }
--- src/main/java/com/example/demo/FeignRun.java
+++ src/main/java/com/example/demo/FeignRun.java
@@ -1,12 +1,9 @@
 package com.example.demo;
 
-import com.example.demo.models.Appointment;
-import com.example.demo.models.Lecturer;
+import com.example.demo.models.Attachment;
 import com.example.demo.models.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Component
 public class FeignRun implements CommandLineRunner {
@@ -21,23 +18,19 @@ public class FeignRun implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         // Registering a student - ID = 5
-        Student reg = feignClient.registerStudent(new Student("94566", "Ian"));
+        Student reg = feignClient.registerStudent(new Student("94337", "Shawn"));
         System.out.println("Created the following: " + reg);
 
-        //Find Registered Student
-        Student find = feignClient.findByName("94566");
-        System.out.println("Found your request: " + find);
+        // Request attachment
+        Attachment request = feignClient.createAttachment(43L);
+        System.out.println(request);
 
-        // Get all lecs
-        List<Lecturer> all = feignClient.getAll();
-        System.out.println("All Lecs: " + all);
+        //Patch Student
+        Attachment patchAttachment = feignClient.patchAttachment(43L, 14L);
 
-        //Book an appointment
-        Appointment schedule = feignClient.book(new Appointment(5L, 1L));
-        System.out.println("Appointment: " + schedule);
+        //Delete Application
+        Attachment deleteAttached = feignClient.deleteAttachment(43L, 14L, 7L);
+        System.out.println(request);
 
-        //Confirm booking
-        Appointment confirmation = feignClient.confirm(5L, 6L);
-        System.out.println("Confirmed: " + confirmation);
     }
 }
